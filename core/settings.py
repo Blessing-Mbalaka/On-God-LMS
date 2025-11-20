9dc917ebd5ac0b64bcfd7a0b4b318939f6c25951
Billie-Matemba <billiematemba133@gmail.com>
2025-11-20 21:52:29 +0200

    test3

--- core/settings.py
+++ core/settings.py
@@ -176,6 +176,161 @@
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+#########################################################################################################
+
+# from pathlib import Path
+# import os
+# from django.contrib.messages import constants as messages
+# import dj_database_url
+
+# GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+# # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+# # Quick-start development settings - unsuitable for production
+# # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-_q5%&jqm$884a0b!an#-we^fx&22(%u+rdp1#7g83on=6f&moa'
+
+# # 🚨 TEMPORARY DEMO CHANGE - Set DEBUG to True
+# DEBUG = True  # Changed from False to True
+
+# # 🚨 TEMPORARY DEMO CHANGE - Allow all hosts for demo
+# ALLOWED_HOSTS = ['*']  # Changed from specific hosts to allow all
+
+# # Application definition
+
+# INSTALLED_APPS = [
+#     'rest_framework',  # required for django rest framework
+#     'widget_tweaks',  # required dependency
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.humanize',
+#     'django.contrib.staticfiles',
+#     'core.apps.CoreConfig',  # To load predefined qualifications
+#     'django_extensions',
+# ]
+
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+#     'whitenoise.middleware.WhiteNoiseMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# ]
+
+# ROOT_URLCONF = 'core.urls'
+
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [BASE_DIR / 'templates'],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+
+# WSGI_APPLICATION = 'core.wsgi.application'
+
+# # Database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'chieta_lmsdb',
+#         'USER': 'chieta_lmsdb_user',
+#         'PASSWORD': 'KLo46ahIw4gYzF3InULyEyd64qA3tWDC',
+#         'HOST': 'dpg-d4e4ut6mcj7s73cfdb6g-a.singapore-postgres.render.com',
+#         'PORT': '5432',
+#     }
+# }
+
+# # Media files
+
+# AWS_ACCESS_KEY_ID = 'AKIA3NHA75JLXPRPPDOM'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'chieta-media'
+# AWS_S3_REGION_NAME = 'us-west-2'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = 'public-read'
+# AWS_QUERYSTRING_AUTH = False
+
+# # Default file storage to S3
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# MEDIA_URL = f'https://chieta-media.s3.amazonaws.com/'
+
+
+# # Static files (CSS, JavaScript, Images)
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# # Password validation
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# ]
+
+# AUTH_USER_MODEL = 'core.CustomUser'
+
+# AUTHENTICATION_BACKENDS = [
+#     'core.authback.EmailBackend',
+#     'django.contrib.auth.backends.ModelBackend',
+# ]
+
+# # Email configuration
+# # 🚨 TEMPORARY DEMO CHANGE - Real email sending for demo
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'stemappza@gmail.com'
+# EMAIL_HOST_PASSWORD = 'ddtz gltz vscj loab'  # Your app password
+# DEFAULT_FROM_EMAIL = 'STEM LMS <stemappza@gmail.com>'
+
+# MESSAGE_TAGS = {
+#     messages.DEBUG: 'secondary',
+#     messages.INFO: 'info',
+#     messages.SUCCESS: 'success',
+#     messages.WARNING: 'warning',
+#     messages.ERROR: 'danger',
+# }
+
+# # Internationalization
+# LANGUAGE_CODE = 'en-us'
+# TIME_ZONE = 'UTC'
+# USE_I18N = True
+# USE_TZ = True
+
+# # Default primary key field type
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#########################################################################################################
+
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
@@ -256,20 +411,11 @@ DATABASES = {
     }
 }
 
-# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-AWS_ACCESS_KEY_ID = 'AKIA3NHA75JLXPRPPDOM'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'chieta-media'
-AWS_S3_REGION_NAME = 'us-west-2'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = 'public-read'
-AWS_QUERYSTRING_AUTH = False
-
-# Default file storage to S3
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = f'https://chieta-media.s3.amazonaws.com/'
-
+# 🚨 TEMPORARY DEMO CHANGE - Create media folder on startup
+os.makedirs(MEDIA_ROOT, exist_ok=True)
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
@@ -300,7 +446,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-# Email configuration
+# Email configuration 
 # 🚨 TEMPORARY DEMO CHANGE - Real email sending for demo
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -326,3 +472,4 @@ USE_TZ = True
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
